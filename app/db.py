5b79a9858c08a49be1cdcd1ca3477f1a2234e1fb
gabriel.lenon <glenonsilva@gmail.com>
2025-09-28 02:04:56 -0300

    feat: add filter for active classes in member queries

--- app/db.py
+++ app/db.py
@@ -20,11 +20,13 @@ def get_members():
 
 @st.cache_data(ttl=300)
 def get_members_classes(member_ids=None):
-    query_get_students = "SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Aluno' AS role FROM core_member JOIN ebd_ebdclass_students ON core_member.id = ebd_ebdclass_students.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_students.ebdclass_id"
+    filter_by_active_classes = "WHERE ebd_ebdclass.is_active = TRUE"
 
-    query_get_secretaries = "SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Secretário' AS role FROM core_member JOIN ebd_ebdclass_secretaries ON core_member.id = ebd_ebdclass_secretaries.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_secretaries.ebdclass_id"
+    query_get_students = f"SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Aluno' AS role FROM core_member JOIN ebd_ebdclass_students ON core_member.id = ebd_ebdclass_students.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_students.ebdclass_id {filter_by_active_classes}"
 
-    query_get_teachers = "SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Professor' AS role FROM core_member JOIN ebd_ebdclass_teachers ON core_member.id = ebd_ebdclass_teachers.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_teachers.ebdclass_id"
+    query_get_secretaries = f"SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Secretário' AS role FROM core_member JOIN ebd_ebdclass_secretaries ON core_member.id = ebd_ebdclass_secretaries.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_secretaries.ebdclass_id {filter_by_active_classes}"
+
+    query_get_teachers = f"SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Professor' AS role FROM core_member JOIN ebd_ebdclass_teachers ON core_member.id = ebd_ebdclass_teachers.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_teachers.ebdclass_id {filter_by_active_classes}"
 
     if member_ids:
         query_get_students += (
