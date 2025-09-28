55f9cbd2620a564b76b5e237011177e850bf6fbd
gabriel.lenon <glenonsilva@gmail.com>
2025-09-28 01:58:42 -0300

    feat: enhance get_members_classes to include roles for students, secretaries, and teachers

--- app/db.py
+++ app/db.py
@@ -20,10 +20,24 @@ def get_members():
 
 @st.cache_data(ttl=300)
 def get_members_classes(member_ids=None):
-    query = "SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name FROM core_member JOIN ebd_ebdclass_students ON core_member.id = ebd_ebdclass_students.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_students.ebdclass_id"
+    query_get_students = "SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Aluno' AS role FROM core_member JOIN ebd_ebdclass_students ON core_member.id = ebd_ebdclass_students.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_students.ebdclass_id"
+
+    query_get_secretaries = "SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Secretário' AS role FROM core_member JOIN ebd_ebdclass_secretaries ON core_member.id = ebd_ebdclass_secretaries.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_secretaries.ebdclass_id"
+
+    query_get_teachers = "SELECT ebd_ebdclass.id AS class_id, ebd_ebdclass.name AS class_name, core_member.id AS member_id, core_member.name AS member_name, 'Professor' AS role FROM core_member JOIN ebd_ebdclass_teachers ON core_member.id = ebd_ebdclass_teachers.member_id JOIN ebd_ebdclass ON ebd_ebdclass.id = ebd_ebdclass_teachers.ebdclass_id"
 
     if member_ids:
-        query += f" WHERE core_member.id IN ({','.join(map(str, member_ids))})"
+        query_get_students += (
+            f" WHERE core_member.id IN ({','.join(map(str, member_ids))})"
+        )
+        query_get_secretaries += (
+            f" WHERE core_member.id IN ({','.join(map(str, member_ids))})"
+        )
+        query_get_teachers += (
+            f" WHERE core_member.id IN ({','.join(map(str, member_ids))})"
+        )
+
+    query = f"({query_get_students}) UNION ({query_get_secretaries}) UNION ({query_get_teachers})"
 
     with engine.connect() as conn:
         return pd.read_sql(query, conn)
